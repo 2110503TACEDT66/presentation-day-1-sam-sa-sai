f2e678a423ce311df78a6409ebe5df1342e86692
KpSuphakorn <suphakorn04413@gmail.com>
2024-03-04 18:16:53 +0700

    make password can input with -

--- models/User.js
+++ models/User.js
@@ -13,10 +13,10 @@ const UserSchema = new mongoose.Schema({
         unique: true,
         validate: {
             validator: function(v) {
-                return /^\d{10}$/.test(v);
+                return /^\d{10}$/.test(v.replace(/-/g, ''));
             },
             message: props => `${props.value} is not a valid phone number! Please enter 10 digits.`
-        }
+        }  
     },
     email: {
         type: String,
